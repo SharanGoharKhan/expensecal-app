515b785a07c12c676f05148a086e1098de7711c5
Sharan Khan <sharan.gohar@gmail.com>
2017-05-17 16:10:49 +0500

    Edit category

--- src/components/public/category/category.js
+++ src/components/public/category/category.js
@@ -5,10 +5,29 @@ app.config(function($stateProvider){
 		.state("app.public.category",{
 			url: "/category",
 			templateUrl: "/templates/components/public/category/category.html",
-			controller: "CategoryController"
+			controller: "CategoryController",
+			resolve: 
+			{
+				$categories: function(Restangular)
+				{
+					return Restangular.all('categories').getList(); 
+				}
+			}
 		})
 }); 
 
-app.controller("CategoryController", function ($scope, $rootScope) {
-	$scope.message="Category controller";
+app.controller("CategoryController", function ($scope, $rootScope,$categories) {
+	$scope.addCategory = function()
+	{
+		var baseCategory = $categories;
+		baseCategory.post($scope.category);
+	}
+	$scope.showCategory = function()
+	{
+		$scope.category=$categories;
+	}
+	$scope.editCategory = function()
+	{
+		console.log("Edit Category called");
+	}
 });
